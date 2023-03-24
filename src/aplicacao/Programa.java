c47ecb44cfa07a55a128db203298cde9c618dc2b
netoitb10 <neto.itaituba@yahoo.com.br>
2023-03-24 11:26:13 -0300

    Primeira classe: Posição

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,16 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.println("Início!");
+		
+		Posicao pos = new Posicao(3, 5);
+		
+		System.out.println(pos);
+
+	}
+
+}
